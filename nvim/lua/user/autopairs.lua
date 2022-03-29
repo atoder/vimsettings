21863a80833d9f90757ec855ffdb75566d8804ad
atoder <alexandert@gmail.com>
2022-03-28 17:49:00 -0700

    changing key mapping for bracket matching

--- nvim/lua/user/autopairs.lua
+++ nvim/lua/user/autopairs.lua
@@ -13,7 +13,7 @@ npairs.setup {
   },
   disable_filetype = { "TelescopePrompt", "spectre_panel" },
   fast_wrap = {
-    map = "<M-e>",
+    map = "<leader>p", -- use this to selectd where to
     chars = { "{", "[", "(", '"', "'" },
     pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
     offset = 0, -- Offset from pattern match
